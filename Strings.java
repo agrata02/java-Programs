e999a7f33fb016921700eee760e554a842792eae
agrata02 <agrata.deepak@gmail.com>
2018-05-30 16:43:44 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    { 
+        Scanner scan = new Scanner(System.in);
+        int testCases = scan.nextInt();
+        for(int i = 0; i < testCases; i++)
+        {
+            char[] inputString = scan.next().toCharArray();
+			
+            // Print even chars
+            for(int j = 0; j < inputString.length; j += 2)
+            {
+                System.out.print(inputString[j]);
+            }
+            System.out.print(" ");
+            
+            // Print odd chars
+            for(int j = 1; j < inputString.length; j += 2)
+            {
+                System.out.print(inputString[j]);
+            }
+            System.out.println();
+      }
+
+    }
+}
